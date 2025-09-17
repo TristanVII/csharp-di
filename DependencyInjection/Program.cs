ad8aeb8bcb76421f79870145ad7c17150aa856df
TristanVII <tristandavis888@gmail.com>
2025-09-16 23:58:25 -0700

    added another example

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -8,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 // When need IgamesRepository, inject InMempryGamesRepository
 builder.Services.AddSingleton<IGamesRepository, InMemoryGamesRepository>();
 
+// Let Di create the class
+// Transient means that it re-creates the instance everytime it is created
+builder.Services.AddTransient<TestDi2>();
+
 var app = builder.Build();
 
 // injects GamesRepo into Map endpoints
@@ -17,7 +21,11 @@ app.CreateGamesEndpoints();
 TestDi test = new(new InMemoryGamesRepository());
 test.PrintGames();
 
-// using scope
+// using scope to grab the InMemoryGamesRepo
 using var scope = app.Services.CreateScope();
 var gamesRepo = scope.ServiceProvider.GetRequiredService<IGamesRepository>();
 gamesRepo.GetAll();
+
+// using Di to create the class that has IGamesRepositoryDi
+var testDi2 = scope.ServiceProvider.GetRequiredService<TestDi2>();
+testDi2.PrintGames();
